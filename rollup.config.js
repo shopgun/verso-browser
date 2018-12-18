cc434c20ffbc57d235e053af60a5d553a1f5c4c3
Morten Bo Rønsholdt <mr@shopgun.com>
2018-12-18 10:35:52 +0100

    Fix Babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,63 @@
-import coffeescript from "rollup-plugin-coffee-script";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
-import path from "path";
-import babel from "rollup-plugin-babel";
+import coffeescript from 'rollup-plugin-coffee-script';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+import path from 'path';
+import babel from 'rollup-plugin-babel';
 import globals from 'rollup-plugin-node-globals';
 
-var input = path.join(__dirname, "lib", "coffeescript", "verso.coffee");
+var input = path.join(__dirname, 'lib', 'coffeescript', 'verso.coffee');
 
 var outputs = {
   // Exclusive bundles(external `require`s untouched), for node, webpack etc.
-  CJS: path.join(__dirname, "dist", "verso.cjs.js"), // CommonJS
-  ES: path.join(__dirname, "dist", "verso.es.js"), // ES Module
+  CJS: path.join(__dirname, 'dist', 'verso.cjs.js'), // CommonJS
+  ES: path.join(__dirname, 'dist', 'verso.es.js'), // ES Module
   // Inclusive bundles(external `require`s resolved), for browsers etc.
-  UMD: path.join(__dirname, "dist", "verso.js"),
-  UMDMin: path.join(__dirname, "dist", "verso.min.js"),
+  UMD: path.join(__dirname, 'dist', 'verso.js'),
+  UMDMin: path.join(__dirname, 'dist', 'verso.min.js'),
 };
+
+const getBabelPlugin = () =>
+  babel({
+    exclude: ['node_modules/**'],
+    extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.coffee']
+  });
 
 let configs = [
   {
     input,
     output: {
       file: outputs.CJS,
-      format: "cjs"
+      format: 'cjs'
     },
     plugins: [
       coffeescript(),
       commonjs({
-        extensions: [".js", ".coffee"]
+        extensions: ['.js', '.coffee']
       }),
-      babel({
-        exclude: 'node_modules/**',
-      })
+      getBabelPlugin()
     ]
   },
   {
     input,
     output: {
       file: outputs.ES,
-      format: "es"
+      format: 'es'
     },
     plugins: [
       coffeescript(),
       commonjs({
-        extensions: [".js", ".coffee"]
+        extensions: ['.js', '.coffee']
       }),
-      babel({
-        exclude: 'node_modules/**',
-      })
+      getBabelPlugin()
     ]
   },
   {
     input,
     output: {
       file: outputs.UMD,
-      format: "umd",
-      name: "Verso"
+      format: 'umd',
+      name: 'verso'
     },
     plugins: [
       coffeescript(),
@@ -66,20 +68,18 @@ let configs = [
         preferBuiltins: false
       }),
       commonjs({
-        extensions: [".js", ".coffee"]
+        extensions: ['.js', '.coffee']
       }),
       globals(),
-      babel({
-        exclude: 'node_modules/**',
-      }),
+      getBabelPlugin()
     ]
   },
   {
     input,
     output: {
       file: outputs.UMDMin,
-      format: "umd",
-      name: "Verso"
+      format: 'umd',
+      name: 'verso'
     },
     plugins: [
       coffeescript(),
@@ -90,18 +90,16 @@ let configs = [
         preferBuiltins: false
       }),
       commonjs({
-        extensions: [".js", ".coffee"]
+        extensions: ['.js', '.coffee']
       }),
       globals(),
-      babel({
-        exclude: 'node_modules/**',
-      }),
+      getBabelPlugin(),
       terser()
     ]
   }
 ];
 // Only output unminified browser bundle in development mode
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   configs = [configs[2]];
 }
 
