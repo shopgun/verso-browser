9b6b69a171a2e89efcf3e3dec209be0141087d94
klarstrup <jonas@klarstrup.dk>
2019-03-27 00:34:42 +0100

    name UMD properly

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import coffeescript from 'rollup-plugin-coffee-script';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import path from 'path';
 import babel from 'rollup-plugin-babel';
 import globals from 'rollup-plugin-node-globals';
@@ -9,98 +9,97 @@ import globals from 'rollup-plugin-node-globals';
 var input = path.join(__dirname, 'lib', 'coffeescript', 'verso.coffee');
 
 var outputs = {
-  // Exclusive bundles(external `require`s untouched), for node, webpack etc.
-  CJS: path.join(__dirname, 'dist', 'verso.cjs.js'), // CommonJS
-  ES: path.join(__dirname, 'dist', 'verso.es.js'), // ES Module
-  // Inclusive bundles(external `require`s resolved), for browsers etc.
-  UMD: path.join(__dirname, 'dist', 'verso.js'),
-  UMDMin: path.join(__dirname, 'dist', 'verso.min.js'),
+    // Exclusive bundles(external `require`s untouched), for node, webpack etc.
+    CJS: path.join(__dirname, 'dist', 'verso.cjs.js'), // CommonJS
+    ES: path.join(__dirname, 'dist', 'verso.es.js'), // ES Module
+    // Inclusive bundles(external `require`s resolved), for browsers etc.
+    UMD: path.join(__dirname, 'dist', 'verso.js'),
+    UMDMin: path.join(__dirname, 'dist', 'verso.min.js')
 };
 
 const getBabelPlugin = () =>
-  babel({
-    exclude: ['node_modules/**'],
-    extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.coffee']
-  });
+    babel({
+        exclude: ['node_modules/**'],
+        extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.coffee']
+    });
 
 let configs = [
-  {
-    input,
-    output: {
-      file: outputs.CJS,
-      format: 'cjs'
+    {
+        input,
+        output: {
+            file: outputs.CJS,
+            format: 'cjs'
+        },
+        plugins: [
+            coffeescript(),
+            commonjs({
+                extensions: ['.js', '.coffee']
+            }),
+            getBabelPlugin()
+        ]
     },
-    plugins: [
-      coffeescript(),
-      commonjs({
-        extensions: ['.js', '.coffee']
-      }),
-      getBabelPlugin()
-    ]
-  },
-  {
-    input,
-    output: {
-      file: outputs.ES,
-      format: 'es'
+    {
+        input,
+        output: {
+            file: outputs.ES,
+            format: 'es'
+        },
+        plugins: [
+            coffeescript(),
+            commonjs({
+                extensions: ['.js', '.coffee']
+            }),
+            getBabelPlugin()
+        ]
     },
-    plugins: [
-      coffeescript(),
-      commonjs({
-        extensions: ['.js', '.coffee']
-      }),
-      getBabelPlugin()
-    ]
-  },
-  {
-    input,
-    output: {
-      file: outputs.UMD,
-      format: 'umd',
-      name: 'verso'
+    {
+        input,
+        output: {
+            file: outputs.UMD,
+            format: 'umd',
+            name: 'Verso'
+        },
+        plugins: [
+            coffeescript(),
+            resolve({
+                jsnext: true,
+                main: true,
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs({
+                extensions: ['.js', '.coffee']
+            }),
+            globals(),
+            getBabelPlugin()
+        ]
     },
-    plugins: [
-      coffeescript(),
-      resolve({
-        jsnext: true,
-        main: true,
-        browser: true,
-        preferBuiltins: false
-      }),
-      commonjs({
-        extensions: ['.js', '.coffee']
-      }),
-      globals(),
-      getBabelPlugin()
-    ]
-  },
-  {
-    input,
-    output: {
-      file: outputs.UMDMin,
-      format: 'umd',
-      name: 'verso'
-    },
-    plugins: [
-      coffeescript(),
-      resolve({
-        jsnext: true,
-        main: true,
-        browser: true,
-        preferBuiltins: false
-      }),
-      commonjs({
-        extensions: ['.js', '.coffee']
-      }),
-      globals(),
-      getBabelPlugin(),
-      terser()
-    ]
-  }
+    {
+        input,
+        output: {
+            file: outputs.UMDMin,
+            format: 'umd',
+            name: 'Verso'
+        },
+        plugins: [
+            coffeescript(),
+            resolve({
+                jsnext: true,
+                main: true,
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs({
+                extensions: ['.js', '.coffee']
+            }),
+            globals(),
+            getBabelPlugin(),
+            terser()
+        ]
+    }
 ];
+
 // Only output unminified browser bundle in development mode
-if (process.env.NODE_ENV === 'development') {
-  configs = [configs[2]];
-}
+if (process.env.NODE_ENV === 'development') configs = [configs[2]];
 
 export default configs;
